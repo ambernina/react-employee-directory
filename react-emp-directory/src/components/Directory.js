c2e066bbfea38aa8f8caa12c03daf496be7cd911
ambernina <rob.ambernina@gmail.com>
2020-03-21 17:48:27 -0600

    got table of random people all working

--- react-emp-directory/src/components/Directory.js
+++ react-emp-directory/src/components/Directory.js
@@ -1,79 +1,105 @@
 import React, { Component } from "react";
 import Container from "./Container";
-import Row from "./Row";
-import Col from "./Col";
-import Card from "./Card";
-import SearchForm from "./SearchForm";
-import MovieDetail from "./MovieDetail";
+// import Row from "./Row";
+// import Col from "./Col";
+// import Card from "./Card";
+import TRow from "./TRow";
+// import SearchForm from "./SearchForm";
 // import API from "../utils/API";
 
+import moment from "moment";
+
 class Directory extends Component {
-  state = {
-    result: {},
-    search: ""
-  };
+	state = {
+		result: [],
+		search: ""
+	};
 
-  // componentDidMount() {
-  //   this.searchMovies("the emperor's new groove");
-  // }
+	componentDidMount() {
+		this.randomPerson();
+		// const res = fetch("https://randomuser.me/api/");
+		// const json = res.json();
+		// console.log("json", json);
+		// this.setState({ result: json });
+	}
 
-  // searchMovies = query => {
-  //   API.search(query)
-  //     .then(res => this.setState({ result: res.data }))
-  //     .catch(err => console.log(err));
-  // };
+	randomPerson = () => {
+		fetch("https://randomuser.me/api/?results=20")
+			.then(res => res.json())
+			.then(response => {
+				console.log("response", response);
+				this.setState({ result: response.results });
+			});
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+		// API.search()
+		//   .then(res => {
+		//     this.setState({ result: res.data.results });
+		//     console.log("result", this.state.result[0]);
+		//     // console.log("res",res);
+		//     // console.log("results", res.data.results);
+		//   })
+		//   .catch(err => console.log(err));
+	};
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    this.searchMovies(this.state.search);
-  }
+	// handleInputChange = event => {
+	//   const { name, value } = event.target;
+	//   this.setState({
+	//     [name]: value
+	//   });
+	// };
 
-  render() {
-    // ternary
-    const movieTitle = this.state.result.Title ? this.state.result.Title : 'No movie title';
-    console.log('movieTitle', movieTitle);
+	// handleFormSubmit = event => {
+	// 	event.preventDefault();
+	// 	this.randomPerson();
+	// };
 
+	formatBD = str => {
+		const newDate = moment(str).format("LL");
+		// console.log("newDate", newDate);
+		return newDate;
+	};
 
-    return (
-      <Container>
-        <Row>
-          <Col size="md-8">
-            <Card
-              heading={this.state.result.Title || "Search for a Movie to Begin"}
-            >
-              {this.state.result.Title ? (
-                <MovieDetail
-                  title={this.state.result.Title}
-                  src={this.state.result.Poster}
-                  director={this.state.result.Director}
-                  genre={this.state.result.Genre}
-                  released={this.state.result.Released}
-                />
-              ) : (
-                <h3>No Results!</h3>
-              )}
-            </Card>
-          </Col>
-          <Col size="md-4">
-            <Card heading="Search">
-              <SearchForm
-                value={this.state.search}
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}
-              />
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+	render() {
+		// ternary
+		console.log("result", this.state.result);
+		return (
+			<Container>
+				<table className="table">
+					<thead>
+						<tr>
+							<th scope="col">Photo</th>
+							<th scope="col">Name</th>
+							<th scope="col">Email</th>
+							<th scope="col">Phone #</th>
+							<th scope="col">Birthdate</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.result.map(person => (
+							<TRow
+								key={person.login.uuid}
+								firstName={person.name.first}
+								lastName={person.name.last}
+								src={person.picture.thumbnail}
+								email={person.email}
+								phone={person.cell}
+								birthdate={this.formatBD(person.dob.date)}
+							/>
+						))}
+					</tbody>
+				</table>
+				{/* <Col size="md-4">
+						<Card>
+							<SearchForm
+								// value={this.state.search}
+								// handleInputChange={this.handleInputChange}
+								handleFormSubmit={this.handleFormSubmit}
+							/>
+						</Card>
+					</Col> */}
+			</Container>
+		);
+	}
 }
 
 export default Directory;
