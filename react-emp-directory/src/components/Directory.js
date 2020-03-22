6e904b48c1ab5110af01bc95e841c1c612f37983
ambernina <rob.ambernina@gmail.com>
2020-03-21 21:43:16 -0600

    tried to add a function to start everything but didnt work so commented out

--- react-emp-directory/src/components/Directory.js
+++ react-emp-directory/src/components/Directory.js
@@ -6,16 +6,24 @@ import SearchBar from "./Search";
 import moment from "moment";
 
 let filtered = false;
+// let started = false;
 
 class Directory extends Component {
 	state = {
 		result: [],
 		filteredResult: [],
-		search: ""
+    search: "",
+    started: false
 	};
 
+	// getStarted = () => {
+    // this.setState({ started: true });
+    // started = true;
+	// };
+  
 	componentDidMount() {
-		this.randomPerson();
+    this.randomPerson();
+		// this.getStarted();
 	}
 
 	randomPerson = () => {
@@ -27,11 +35,6 @@ class Directory extends Component {
 			});
 	};
 
-	// handleFormSubmit = event => {
-	// 	event.preventDefault();
-	// 	this.randomPerson();
-	// };
-
 	globalSearch = () => {
 		let { search, result } = this.state;
 		console.log("search", search);
@@ -39,18 +42,18 @@ class Directory extends Component {
 		let filteredResult = result.filter(value => {
 			return (
 				value.name.first.toLowerCase().includes(search.toLowerCase()) ||
-        value.name.last.toLowerCase().includes(search.toLowerCase()) || 
-        value.email.toLowerCase().includes(search.toLowerCase())
+				value.name.last.toLowerCase().includes(search.toLowerCase()) ||
+				value.email.toLowerCase().includes(search.toLowerCase())
 			);
 		});
 		this.setState({ filteredResult });
 		console.log("filtered result", filteredResult);
-  };
-  
-  handleChange = event => {
+	};
+
+	handleChange = event => {
 		this.setState({ search: event.target.value }, () => {
-      this.globalSearch();
-      filtered = true;
+			this.globalSearch();
+			filtered = true;
 		});
 	};
 
@@ -62,62 +65,83 @@ class Directory extends Component {
 
 	render() {
 		// ternary
-		// console.log("result", this.state.result);
+		console.log("started", this.state.started);
 		return (
-			// <div>
-				<Container>
-				<div className="jumbotron">
-					<h1 className="display-4">Employee Directory</h1>
-					<p className="lead">
-						Welcome to your Employee Directory! To find someone specific, simply
-						type in the search bar to narrow the list down.
-					</p>
-					<hr className="my-4" />
-					<SearchBar
-						name="search"
-						value={this.state.search || ""}
-						onChange={this.handleChange}
-						label="Search"
-					/>
-				</div>
-					<table className="table">
-						<thead>
-							<tr>
-								<th scope="col">Photo</th>
-								<th scope="col">Name</th>
-								<th scope="col">Email</th>
-								<th scope="col">Phone #</th>
-								<th scope="col">Birthdate</th>
-							</tr>
-						</thead>
-					<tbody>
-						{!filtered
-							? this.state.result.map(person => (
-									<TRow
-										key={person.login.uuid}
-										firstName={person.name.first}
-										lastName={person.name.last}
-										src={person.picture.thumbnail}
-										email={person.email}
-										phone={person.cell}
-										birthdate={this.formatBD(person.dob.date)}
-									/>
-							  ))
-							: this.state.filteredResult.map(person => (
-									<TRow
-										key={person.login.uuid}
-										firstName={person.name.first}
-										lastName={person.name.last}
-										src={person.picture.thumbnail}
-										email={person.email}
-										phone={person.cell}
-										birthdate={this.formatBD(person.dob.date)}
-									/>
-							  ))}
-					</tbody>
-				</table>
-			</Container>
-      // </div>
+			<div className="container">
+				{/* {!this.state.started ? (
+					<div className="jumbotron text-center">
+						<h1 className="display-4">Employee Directory</h1>
+						<p className="lead">
+							Welcome to your Employee Directory! To find someone specific,{" "}
+							<br />
+							simply type in the search bar to narrow the list down.
+						</p>
+						<hr className="my-4" />
+
+						<button
+							type="button"
+							className="btn btn-info"
+							onChange={this.getStarted()}
+						>
+							Get Started!
+						</button>
+					</div>
+				) : ( */}
+					<Container>
+						<div className="jumbotron text-center">
+							<h1 className="display-4">Employee Directory</h1>
+							<p className="lead">
+								Welcome to your Employee Directory! To find someone specific,{" "}
+								<br />
+								simply type in the search bar to narrow the list down.
+							</p>
+							<hr className="my-4" />
+							<SearchBar
+								name="search"
+								value={this.state.search || ""}
+								onChange={this.handleChange}
+								label="Search"
+							/>
+						</div>
+						<table className="table">
+							<thead>
+								<tr>
+									<th scope="col">Photo</th>
+									<th scope="col">Name</th>
+									<th scope="col">Email</th>
+									<th scope="col">Phone #</th>
+									<th scope="col">Birthdate</th>
+								</tr>
+							</thead>
+							<tbody>
+								{!filtered
+									? this.state.result.map(person => (
+											<TRow
+												key={person.login.uuid}
+												firstName={person.name.first}
+												lastName={person.name.last}
+												src={person.picture.thumbnail}
+												email={person.email}
+												phone={person.cell}
+												birthdate={this.formatBD(person.dob.date)}
+											/>
+									  ))
+									: this.state.filteredResult.map(person => (
+											<TRow
+												key={person.login.uuid}
+												firstName={person.name.first}
+												lastName={person.name.last}
+												src={person.picture.thumbnail}
+												email={person.email}
+												phone={person.cell}
+												birthdate={this.formatBD(person.dob.date)}
+											/>
+									  ))}
+							</tbody>
+						</table>
+					</Container>
+				{/* )} */}
+			</div>
 		);
 	}
 }
